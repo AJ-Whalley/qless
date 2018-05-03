4c22dfd32febe749b3fb3cf5b765dc943b1d592d
Andrew Whalley <andrewcwhalley@gmail.com>
2018-05-03 21:20:56 +1000

    Add geocoding to profile model

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,4 +1,23 @@
 class Profile < ApplicationRecord
   include ImageUploader::Attachment.new(:image) # adds an `image` virtual attribute
   belongs_to :user
+
+  validates(
+    :first_name,
+    :last_name,
+    :street_address,
+    :city,
+    :state,
+    :postal_code,
+    :country_code,
+    presence: true) 
+
+  geocoded_by :full_address
+  after_validation :geocode
+
+def full_address
+  "#{street_address}, #{city}, #{state}, #{postal_code}, #{country_code},"
+end 
+
+
 end
