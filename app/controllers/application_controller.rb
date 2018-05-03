df6334d0e3351d7c387b69388cf777ddbf62a200
Andrew Whalley <andrewcwhalley@gmail.com>
2018-05-03 19:53:26 +1000

    Create shrine.rb initializer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,18 @@ class ApplicationController < ActionController::Base
   include Pundit
   protect_from_forgery with: :exception
   before_action :authenticate_user!, except: [:index, :support, :how_it_works]
+
+
+  def after_sign_in_path_for(user)
+    if user.profile.nil?
+      new_profile_path
+    else 
+      pages_user_home_path  
+    end 
+  end
+  
+
+
+
 end
 
