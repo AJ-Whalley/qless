12671c2df95036a65087538d7eed9c629d861919
Andrew Whalley <andrewcwhalley@gmail.com>
2018-05-04 20:12:27 +1000

    Edit links to show jobs via user home

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,4 +1,17 @@
 class Job < ApplicationRecord
   belongs_to :user
   has_many :applications
+
+  validates(
+    :street_address,
+    :city,
+    presence: true) 
+
+  geocoded_by :full_address
+  after_validation :geocode
+
+def full_address
+  "#{street_address}, #{city}"
+end 
+
 end
